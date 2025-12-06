c2c081f4528dbb98bc368299fd417ba9613ccaf2
phoemsophanna <phannaphoem@gmail.com>
2025-12-06 16:59:21 +0700

    Update tiktok

--- src/pages/Pages/Project/ProjectForm.js
+++ src/pages/Pages/Project/ProjectForm.js
@@ -122,6 +122,7 @@ const ProjectForm = (props) => {
 			facebookLink: project ? project.facebookLink : "",
 			instagramLink: project ? project.instagramLink : "",
 			telegramLink: project ? project.telegramLink : "",
+			tiktokLink: project ? project.tiktokLink : "",
 			appStore: project ? project.appStore : "",
 			playStore: project ? project.playStore : "",
 			ordering: project ? project.ordering : 0,
@@ -335,6 +336,28 @@ const ProjectForm = (props) => {
 												value={projectValidation.values.playStore || ""}
 											/>
 											<i className="ri-google-play-line"></i>
+										</div>
+										<div className="form-icon mb-3">
+											<Input
+												type="text"
+												className="form-control form-control-icon"
+												placeholder="Enter link"
+												name="tiktokLink"
+												onChange={projectValidation.handleChange}
+												onBlur={projectValidation.handleBlur}
+												value={projectValidation.values.tiktokLink || ""}
+											/>
+											<svg 
+												style={{
+													position: "absolute",
+													top: "10px",
+													bottom: "0px",
+													left: "18px",
+													display: "flex",
+													alignItems: "center",
+													width: "15px"
+												}}
+											xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M16 8.24537V15.5C16 19.0899 13.0899 22 9.5 22C5.91015 22 3 19.0899 3 15.5C3 11.9101 5.91015 9 9.5 9C10.0163 9 10.5185 9.06019 11 9.17393V12.3368C10.5454 12.1208 10.0368 12 9.5 12C7.567 12 6 13.567 6 15.5C6 17.433 7.567 19 9.5 19C11.433 19 13 17.433 13 15.5V2H16C16 4.76142 18.2386 7 21 7V10C19.1081 10 17.3696 9.34328 16 8.24537Z"></path></svg>
 										</div>
 									</CardBody>
 								</Card>
